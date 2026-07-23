a1abeeaa6daa704b6cddd417201e087361fdf0b9
Heng-Kai-Sheng05 <kaisheng.heng@qiu.edu.my>
2026-07-23 13:47:57 +0800

    Second commit

--- hw.cpp
+++ hw.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
+    // This is a simple C++ program that prints "Hello, world!" to the console.
     cout << "Hello, world!" << endl;
     return 0;
 }
